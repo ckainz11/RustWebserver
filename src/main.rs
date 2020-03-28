31ddb53398a7e1f2063ccbf82175d28720054a08
Christoph <ckain11@gmx.at>
2020-03-28 18:49:24 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("192.168.0.139:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
